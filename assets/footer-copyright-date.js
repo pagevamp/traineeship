b18862e3f0f3a9d3badbc19b24f2b3a0d7896ff8
SushilSharmaSubedi <sushil.subedi@outside.tech>
2025-01-30 15:18:10 +0545

    Improvisation :: Improvised get current year using liquid

--- assets/footer-copyright-date.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const copyrightElements = document.getElementsByClassName("copyright-date");
-  const currentYear = new Date().getFullYear();
-
-  for (const element of copyrightElements) {
-    const additionalText = element.getAttribute("data-text") || "";
-    element.innerHTML = `&copy; ${currentYear} ${additionalText}`;
-  }
-});
